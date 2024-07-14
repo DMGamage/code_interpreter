d22809a33f042b28648c5d6cc44230b3de72b7cf
DMGamage <gamagemadhushan@gmail.com>
2024-07-14 15:44:36 +0530

    create Router agent select csv_agent or python_agent to identify user inpput prompt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_experimental.agents import create_csv_agent
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain import Tool
 
 load_dotenv()
 
@@ -26,38 +27,58 @@ if __name__ == "__main__":
         tools=tools,
     )
 
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    python_agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    # agent_executor.invoke(
-    #     input={
-    #         "input": """
-    #         Generate and save in the current directory 15 QR codes
-    #         that point to www.Github.com or https://gist.github.com,
-    #         assuming you have the qrcode package installed already.
-    #         """
-    #     }
-    # )
-
-    # agent_executor.invoke(
-    #     input={
-    #         "input": """
-    #         Generate and save in the current directory 15 QR codes
-    #         that point to www.Github.com or https://gist.github.com,
-    #         assuming you have the qrcode package installed already.
-    #         """,
-    #         "instructions": instructions,  # Add the instructions variable here
-    #     }
-    # )
+    python_agent_executor.invoke(
+        input={
+            "input": """
+            Generate and save in the current directory 15 QR codes
+            that point to www.Github.com or https://gist.github.com,
+            assuming you have the qrcode package installed already.
+            """,
+            "instructions": instructions,  # Add the instructions variable here
+        }
+    )
+    ### Comment this agent if you need run Router Agent.
 
     csv_agent = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
         path="Car_details.csv",
         verbose=True,
-        allow_dangerous_code=True
+        allow_dangerous_code=True,
     )
 
-    csv_agent.invoke(
-        input={
-            "input":"How many car are there Hyundai name"
-        }
+    csv_agent.invoke(input={"input": "How many car are there Hyundai name"})
+    ### Comment this agent if you need run Router Agent.
+
+    ############# Router Agent ###################
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor.invoke,
+            description=""" Useful  when you need to transform natural language to python and execute the python code,
+            returning the result of your code execution
+            DOES NOT ACCEPT CODE AS INPUT
+            """
+        ),
+        Tool(
+            name="CSV agent",
+            func=csv_agent.invoke,
+            description=""" Useful  when you need to answer question over Car_details.csv file,
+            takes an input the entire question and answer after running pandas calculations.
+               """
+        )
+    ]
+
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools
     )
+
+    grand_agent_executor = AgentExecutor(agent=grand_agent,tools=tools,verbose=True)
+
+    print(grand_agent_executor.invoke(input={"input": "How many car are"}))
